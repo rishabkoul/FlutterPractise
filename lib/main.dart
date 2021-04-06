f6f3d458ca2d7074d1d4dfc5e789d47f78776656
Rishab Koul <rishabkoul2001@gmail.com>
2021-04-06 14:45:21 +0530

    ch2complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,40 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Awesome App"),
         ),
-        body: Container(
-          child: Center(child: Text("Hi Flutter")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Colors.black,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  )
+                ],
+              ),
+            ),
+          ),
         ));
   }
 }
