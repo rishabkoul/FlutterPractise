ffefdb1c93b4a17bc71b78836ea49c64521a3ef4
Rishab Koul <rishabkoul2001@gmail.com>
2021-04-07 13:05:15 +0530

    strem builder done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/pages/home_page_with_fb.dart';
+import 'package:hello_world/pages/home_page_with_strem.dart';
 import 'package:hello_world/pages/login_page.dart';
 import 'package:hello_world/utils/Constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,13 +13,13 @@ Future main() async {
       debugShowCheckedModeBanner: false,
       title: "Awesome App",
       home: Constants.prefs.getBool("LoggedIn") == true
-          ? HomePageFB()
+          ? HomePageSB()
           : LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
       routes: {
         "/login": (context) => LoginPage(),
-        "/home": (context) => HomePageFB(),
+        "/home": (context) => HomePageSB(),
       }));
 }
---
+++ lib/pages/home_page_with_strem.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/utils/Constants.dart';
+import 'package:hello_world/widgets/drawer.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+class HomePageSB extends StatelessWidget {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getData();
+  // }
+
+  Future getData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+    var data;
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    return data;
+  }
+
+  Stream<List<String>> getStreamData() {
+    var data = Stream<List<String>>.fromIterable(
+        [List<String>.generate(20, (index) => "Item $index")]);
+    return data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome App"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                Constants.prefs.setBool("LoggedIn", false);
+                Navigator.pushReplacementNamed(context, '/login');
+              })
+        ],
+      ),
+      body: StreamBuilder(
+        stream: getStreamData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Center(
+                child: Text("Fetch Someting"),
+              );
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.done:
+              print(snapshot.hasError);
+              if (snapshot.hasError) {
+                print('hello');
+                return Center(child: Text("Some error has Occured"));
+              }
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(snapshot.data[index]),
+                    ),
+                  );
+                },
+                itemCount: snapshot.data.length,
+              );
+          }
+          return null;
+        },
+      ),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // myText = _nameController.text;
+          // setState(() {});
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
